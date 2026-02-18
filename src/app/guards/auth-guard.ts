47e8cba27a795720281588c5a20ec9807794f895
Mick-dud <mba.arevalosabando@gmail.com>
2026-02-18 08:49:52 -0500

    login - inicio

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,6 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
 //route es la ruta a la que se quiere acceder, state es el estado de la ruta, se puede usar para redirigir al usuario a otra ruta 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+
+  const servicioAuth =  inject(AuthService);
+  const router = inject(Router);
+
+  if (servicioAuth.sesionIniciada()){
+    return true;
+  }
+  return router.parseUrl('login');
 };
